b4c97b6d8b40461f21765a0477ea08e3eff9df35
Abdeladim <abdeladimdemnati3@gmail.com>
2026-07-15 12:21:37 +0100

    fix: add Heroku deployment config (Procfile, gunicorn, postgres, runtime.txt)

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn gespresence.wsgi --log-file -
--- gespresence/settings.py
+++ gespresence/settings.py
@@ -12,13 +12,14 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-changeme')
-DEBUG = config('DJANGO_DEBUG', default=True, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost').split(',')
+DEBUG = config('DJANGO_DEBUG', default=False, cast=bool)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost,.herokuapp.com').split(',')
 
 
 # Application definition
@@ -79,18 +80,16 @@ WSGI_APPLICATION = 'gespresence.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DB_NAME', default='gespresence_db'),
-        'USER': config('DB_USER', default='root'),
-        'PASSWORD': config('DB_PASSWORD', default=''),
-        'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', default='3306'),
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
-    }
+    'default': dj_database_url.config(
+        default='mysql://{user}:{password}@{host}:{port}/{name}'.format(
+            user=config('DB_USER', default='root'),
+            password=config('DB_PASSWORD', default=''),
+            host=config('DB_HOST', default='localhost'),
+            port=config('DB_PORT', default='3306'),
+            name=config('DB_NAME', default='gespresence_db'),
+        ),
+        conn_max_age=600,
+    )
 }
 
 
